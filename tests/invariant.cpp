ca0954afc107477dfc8af4cbb147f319d4233b5b
martinevsky <max@martinevsky.ru>
2017-09-05 10:22:38 +0300

    Format fix.

--- tests/invariant.cpp
+++ tests/invariant.cpp
@@ -12,32 +12,32 @@ const char* const c_method = "meth";
 
 struct Logic
 {
-	Logic()
+    Logic()
     {
         op (c_ctor);
     }
 
-	Logic(Logic&&)
-	{
-		op (c_mtor);
-	}
+    Logic(Logic&&)
+    {
+        op (c_mtor);
+    }
 
-	~Logic()
-	{
-		op (c_dtor);
-	}
+    ~Logic()
+    {
+        op (c_dtor);
+    }
 
 public:
-	void method()
-	{
-		op (c_method);
-	}
+    void method()
+    {
+        op (c_method);
+    }
 
 protected:
-	void invariant()
-	{
-		op (c_invariant);
-	}
+    void invariant()
+    {
+        op (c_invariant);
+    }
 };
 
 DECL_ADAPTER (Logic, method)
@@ -46,28 +46,28 @@ BOOST_FIXTURE_TEST_SUITE(invariant, OpsFixture)
 
 BOOST_AUTO_TEST_CASE(Method)
 {
-	{
-		AdaptedChecked<Logic> logic;
-		CHECK_OPS (c_ctor, c_invariant);
+    {
+        AdaptedChecked<Logic> logic;
+        CHECK_OPS (c_ctor, c_invariant);
 
-		logic.method();
-		CHECK_OPS (c_ctor, c_invariant, c_invariant, c_method, c_invariant);
-	}
+        logic.method();
+        CHECK_OPS (c_ctor, c_invariant, c_invariant, c_method, c_invariant);
+    }
 
-	CHECK_OPS (c_ctor, c_invariant, c_invariant, c_method, c_invariant, c_invariant, c_dtor);
+    CHECK_OPS (c_ctor, c_invariant, c_invariant, c_method, c_invariant, c_invariant, c_dtor);
 }
 
 BOOST_AUTO_TEST_CASE(MoveCtor)
 {
-	{
-		AdaptedChecked<Logic> logic1;
-		CHECK_OPS (c_ctor, c_invariant);
+    {
+        AdaptedChecked<Logic> logic1;
+        CHECK_OPS (c_ctor, c_invariant);
 
-		AdaptedChecked<Logic> logic2 = std::move (logic1);
-		CHECK_OPS (c_ctor, c_invariant, c_mtor, c_invariant, c_invariant);
-	}
+        AdaptedChecked<Logic> logic2 = std::move (logic1);
+        CHECK_OPS (c_ctor, c_invariant, c_mtor, c_invariant, c_invariant);
+    }
 
-	CHECK_OPS (c_ctor, c_invariant, c_mtor, c_invariant, c_invariant, c_invariant, c_dtor, c_invariant, c_dtor);
+    CHECK_OPS (c_ctor, c_invariant, c_mtor, c_invariant, c_invariant, c_invariant, c_dtor, c_invariant, c_dtor);
 }
 
 BOOST_AUTO_TEST_SUITE_END()
